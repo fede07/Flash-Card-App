5e1c3231db19079800786c82989bf9165bd093ad
Federico Rossendy <fede07@users.noreply.github.com>
2023-10-18 19:45:26 -0300

    Added flipping cards

--- main.py
+++ main.py
@@ -1,21 +1,37 @@
 from cgitb import text
+from email.mime import image
 import random
+from textwrap import fill
+from numpy import flip
 import pandas
 from tkinter import Tk, Canvas, Button, PhotoImage
 
 BACKGROUND_COLOR = "#B1DDC6"
 
 data = pandas.read_csv(r"data\french_words.csv")
-
 to_learn = data.to_dict(orient="records")
 
-def next_card():
-    entry = random.choice(to_learn)
-    french_word = entry["French"]
-    english_word = entry["English"]
+current_card = {}
 
-    canvas.itemconfig(card_title, text="French")
-    canvas.itemconfig(word_text, text=french_word)
+def next_card():
+    global current_card, flip_timer
+    window.after_cancel(flip_timer)
+
+    current_card = random.choice(to_learn)
+    french_word = current_card["French"]
+
+    canvas.itemconfig(card_title, text="French", fill="black")
+    canvas.itemconfig(card_word, text=french_word, fill="black")
+    canvas.itemconfig(card_background, image=card_front_image)
+
+    flip_timer = window.after(3000, func=flip_card)
+
+def flip_card():
+    canvas.itemconfig(card_title, text="English", fill="white")
+    canvas.itemconfig(card_word, text=current_card["English"], fill="white")
+    canvas.itemconfig(card_background, image=card_back_image)
+
+
 
 #--------------UI
 
@@ -23,13 +39,15 @@ window = Tk()
 window.title("Flashy")
 window.config(padx=50, pady=50, background=BACKGROUND_COLOR)
 
+flip_timer = window.after(3000, func=flip_card)
+
 canvas= Canvas(height=526, width=800)
 card_front_image = PhotoImage(file=r"images\card_front.png")
 card_back_image = PhotoImage(file=r"images\card_back.png")
-canvas.create_image(400, 263, image=card_front_image)
+card_background = canvas.create_image(400, 263, image=card_front_image)
 
 card_title = canvas.create_text(400, 150, text="Title", font=("Arial", 40, "italic"))
-word_text = canvas.create_text(400, 263, text="Word", font=("Arial", 60, "bold"))
+card_word = canvas.create_text(400, 263, text="Word", font=("Arial", 60, "bold"))
 
 canvas.config(bg=BACKGROUND_COLOR, highlightthickness=0)
 canvas.grid(column=0, row=0, columnspan=2)
